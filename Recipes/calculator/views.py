a6f3d1f7b1cff02bfcc74f4dddf0363c4d60e48f
Rassolchick <113520657+Rassolchick@users.noreply.github.com>
2023-04-18 22:35:23 +0300

    Add files via upload

---
+++ Recipes/calculator/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+
+def get_dish(reqeust, dish):
+    """
+    Функция запрашивает название блюда,
+    возвращает render для шаблона с расчетом ингредиентов для нужного количества людей.
+    """
+
+    how_many = int(reqeust.GET.get('servings', 1))
+    context = {'recipe': DATA[dish]}
+    for ingr, quan in context['recipe'].items():
+        context['recipe'][ingr] = quan * how_many
+
+    return render(reqeust, 'calculator/index.html', context=context)
+
+
+def choice_dish(request):
+    """Функия возвращает HttpTesponse c инструкцией по применению калькулятора блюд."""
+
+    return HttpResponse(f'Внесите в путь название блюда из данного списка: {", ".join(DATA.keys())}.'
+                        f' Пример /название блюда/?servings=количество персон')
+
+
+# первые функции, неоптимальный вариант
+#
+# def get_omlet(reqeust):
+#     """"""
+#     how_many = int(reqeust.GET.get('servings', 1))
+#     context = {'recipe': DATA['omlet']}
+#     for ingr, quan in context['recipe'].items():
+#         context['recipe'][ingr] = quan * how_many
+#
+#     return render(reqeust, 'calculator/index.html', context=context)
+#
+#
+# def get_pasta(reqeust):
+#     """"""
+#     how_many = int(reqeust.GET.get('servings', 1))
+#     context = {'recipe': DATA['pasta']}
+#     for ingr, quan in context['recipe'].items():
+#         context['recipe'][ingr] = quan * how_many
+#
+#     return render(reqeust, 'calculator/index.html', context=context)
+#
+#
+# def get_buter(reqeust):
+#     """"""
+#     how_many = int(reqeust.GET.get('servings', 1))
+#     context = {'recipe': DATA['buter']}
+#     for ingr, quan in context['recipe'].items():
+#         context['recipe'][ingr] = quan * how_many
+#
+#     return render(reqeust, 'calculator/index.html', context=context)
+
+
+
---
+++ Recipes/recipes/urls.py
@@ -0,0 +1,29 @@
+"""recipes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+
+from calculator.views import get_dish, choice_dish  # get_omlet, get_pasta, get_buter,
+
+urlpatterns = [
+    # здесь зарегистрируйте вашу view-функцию
+    path('', choice_dish),
+    # path('omlet/', get_omlet),
+    # path('pasta/', get_pasta),
+    # path('buter/', get_buter),
+    path('<dish>/', get_dish),
+    ]
+
